b7a1b07f0171859bb2b845102e11c7dfca901717
dennis <dennis_kleber@mailbox.org>
2020-04-20 13:43:35 +0200

    install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -3,5 +3,4 @@
 go get github.com/labstack/echo/... &&
 go get github.com/gocraft/dbr/... &&
 go get github.com/jinzhu/configor/... &&
-go get github.com/go-playground/validator/... &&
-
+go get github.com/go-playground/validator/...
