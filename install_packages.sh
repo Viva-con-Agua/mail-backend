1a1c65080756e2faf7f88d7c97c0b305435b26cf
Dennis Kleber <dennis_kleber@mailbox.org>
2020-07-08 14:19:07 +0200

    install

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 go get github.com/nats-io/nats.go/... &&
 go get github.com/labstack/echo/... &&
-go get github.com/jinzhu/configor/... 
+go get github.com/jinzhu/configor/... &&
+go get github.com/go-playground/validator/...
